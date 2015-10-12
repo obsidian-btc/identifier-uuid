064edeecf74074ac437954baf42a2bf5c99a4431
Scott Bellware <sbellware@gmail.com>
2015-10-12 13:29:47 -0500

    Sketch implementation of UUID detection

--- lib/identifier/uuid/uuid.rb
+++ lib/identifier/uuid/uuid.rb
@@ -8,6 +8,21 @@ module Identifier
       Random.get
     end
 
+    def self.uuid?(text)
+      uuid = get(text).to_s
+      uuid == text
+    end
+
+    def self.get(text)
+      uuid = stream_name.match(pattern).to_s
+      uuid = nil if uuid == ''
+      uuid
+    end
+
+    def self.pattern
+      /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/i
+    end
+
     def self.zero
       '00000000-0000-0000-0000-000000000000'
     end
