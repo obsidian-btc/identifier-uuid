8fbe1ecf428ef13e92517dc5e3b353f5781ac652
Scott Bellware <sbellware@gmail.com>
2015-10-14 12:24:43 -0500

    Pattern is in its own file

--- lib/identifier/uuid/uuid.rb
+++ lib/identifier/uuid/uuid.rb
@@ -26,19 +26,5 @@ module Identifier
     def self.zero
       '00000000-0000-0000-0000-000000000000'
     end
-
-    module Pattern
-      TYPE_4 = %r{
-        [a-f0-9]{8}          # abcdef01
-        -                    #         -
-        [a-f0-9]{4}          #          abcd
-        -                    #              -
-        4[a-f0-9]{3}         #               4abc
-        -                    #                   -
-        [89ab][a-f0-9]{3}    #                    8abc
-        -                    #                        -
-        [a-f0-9]{12}         #                         abcdef012345
-      }xi
-    end
   end
 end
