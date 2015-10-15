77352efaea64761ce3dd35fd46a363fd42bc4188
Scott Bellware <sbellware@gmail.com>
2015-10-15 13:31:21 -0500

    Sampling the third prefix of UUID is optional

--- lib/identifier/uuid/controls/incrementing.rb
+++ lib/identifier/uuid/controls/incrementing.rb
@@ -2,12 +2,19 @@ module Identifier
   module UUID
     module Controls
       module Incrementing
-        def self.example(i=nil)
+        def self.example(i=nil, sample: nil)
           i ||= 1
+          sample ||= false
 
           first_octet = (i).to_s.rjust(8, '0')
 
-          third_prefix = ['8', '9', 'a', 'b'].sample
+          third_prefixes = ['8', '9', 'a', 'b']
+
+          if sample
+            third_prefix = third_prefixes.sample
+          else
+            third_prefix = third_prefixes[0]
+          end
 
           "#{first_octet}-4000-#{third_prefix}000-0000-000000000000"
         end
