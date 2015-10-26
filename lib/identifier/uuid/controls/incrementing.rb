6136304ba8ba3d5bc2875abdeea82748b06c8312
ntl <nathanladd+github@gmail.com>
2015-10-26 11:51:47 -0500

    Good catch, Scott!

--- lib/identifier/uuid/controls/incrementing.rb
+++ lib/identifier/uuid/controls/incrementing.rb
@@ -8,15 +8,15 @@ module Identifier
 
           first_octet = (i).to_s.rjust(8, '0')
 
-          third_prefixes = ['8', '9', 'a', 'b']
+          fourth_prefixes = ['8', '9', 'a', 'b']
 
           if sample
-            third_prefix = third_prefixes.sample
+            fourth_prefix = fourth_prefixes.sample
           else
-            third_prefix = third_prefixes[0]
+            fourth_prefix = fourth_prefixes[0]
           end
 
-          "#{first_octet}-0000-4000-#{third_prefix}000-000000000000"
+          "#{first_octet}-0000-4000-#{fourth_prefix}000-000000000000"
         end
       end
     end
