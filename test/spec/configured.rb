e8e123c178b8db44e4aeada00c057328799c4562
Scott Bellware <sbellware@gmail.com>
2015-11-23 15:16:30 -0600

    Test coverage for configuration is improved

--- test/spec/configured.rb
+++ test/spec/configured.rb
@@ -12,6 +12,13 @@ module Configured
       attr_accessor :identifier
     end
   end
+
+  module Both
+    class Example
+      attr_accessor :uuid
+      attr_accessor :identifier
+    end
+  end
 end
 
 describe "Configured Receiver" do
@@ -38,6 +45,21 @@ describe "Configured Receiver" do
 
     specify "Receiver has a random UUID generator" do
       assert(identifier.instance_of? Identifier::UUID::Random)
+    end
+  end
+
+  context "Receiver has both uuid and identifier" do
+    let(:receiver) { Configured::Both::Example.new }
+    let(:uuid) { receiver.uuid }
+    let(:identifier) { receiver.identifier }
+
+    before do
+      Identifier::UUID::Random.configure receiver
+    end
+
+    specify "Precedence is given to identifier over uuid" do
+      assert(identifier.instance_of? Identifier::UUID::Random)
+      assert(uuid.nil?)
     end
   end
 end
